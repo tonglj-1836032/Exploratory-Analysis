f762fec2476ce56da1d73aa565519e0e0bda8cbe
xuewenxi <72558164+xuewenxi@users.noreply.github.com>
2020-11-13 03:49:41 -0800

    Update Summary Information.R

--- script/Summary Information.R
+++ script/Summary Information.R
@@ -41,20 +41,65 @@ summary_info$hosp_y <- covid_hosp %>%
   filter(hosp_yn == "Yes") %>%
   nrow() / nrow(covid_hosp)
 
-#The highest mortality rate for male
-r format(max_M,nsmall=2)
+####chart_3
 
+
+covid_death <- COVID %>%
+  filter(death_yn != "Unknown" & death_yn != "Missing")
+  
+covid_death<-covid_death[,c("cdc_report_dt", "sex" ,"age_group","death_yn")]
+
+covid_death <- covid_death %>%
+  filter(sex != "Unknown" & sex != "Missing" )
+covid_death <- covid_death %>%
+  filter(age_group != "Unknown" & age_group != "Missing")
+ 
+age_group=unique(as.character(covid_death$age_group)) 
+sex=unique(as.character(covid_death$sex)) 
+n=length(age_group)
+res=c()
+for(i in 1:n){
+ss=which(as.character(covid_death$age_group)==age_group[i])
+dd=covid_death[ss,]
+m=length(sex)
+rr=c()
+for(j in 1:m)
+{
+dm=length(which(as.character(dd$sex)==sex[j] & as.character(dd$death_yn)=="Yes"))*100/length(ss)
+re=c(sex[j],round(dm,2))
+rr=rbind(rr,re)
+}
+re=cbind(age_group[i],rr)
+res=rbind(res,re)
+}
+colnames(res)=c("Age_group","Sex","Death_yn")
+res=data.frame(res)
+
+
+#The highest mortality rate for male
+
+summary_info$max_M<- res %>%
+  filter(Sex == "Male") %>%
+  filter(Death_yn == max(Death_yn))  %>%
+  pull(Death_yn)
 #The age group with the highest mortality rate of male
-r format(max_M_A)
+summary_info$max_M_A<- res %>%
+  filter(Sex == "Male") %>%
+  filter(Death_yn == max(Death_yn))  %>%
+  pull(age_group)
 
 #The highest mortality rate for female
-r format(max_F,nsmall=2)
+summary_info$max_F<- res %>%
+  filter(Sex == "Fmale") %>%
+  filter(Death_yn == max(Death_yn))  %>%
+  pull(Death_yn)
+
 
 #The age group with the highest mortality rate of female
-r format(max_F_A)
-
-#The mortality rate for each group in each gender
-r format(res$Death_yn,nsmall=2)
+summary_info$max_F_A<- res %>%
+  filter(Sex == "Fmale") %>%
+  filter(Death_yn == max(Death_yn))  %>%
+  pull(age_group)
 
 # Style check
 lint("Summary Information.R")
